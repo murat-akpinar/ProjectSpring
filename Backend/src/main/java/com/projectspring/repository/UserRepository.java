3301c8db22d006b73596d920765990ea815e5d92
Murat AKPINAR <akpinarmurat@protonmail.com>
2026-02-23 22:59:24 +0300

    bugfix:  roles

--- Backend/src/main/java/com/projectspring/repository/UserRepository.java
+++ Backend/src/main/java/com/projectspring/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.projectspring.repository;
 
 import com.projectspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLdapDn(String ldapDn);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN u.teams t WHERE t.id = :teamId AND u.isActive = true")
+    List<User> findByTeamId(@Param("teamId") Long teamId);
 }
 
